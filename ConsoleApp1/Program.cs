4a5655019406f332e3968f84044732c8b678777c
010101000110111101110010 <maks.ogloblin.2002@mail.ru>
2020-10-14 18:26:14 +0900

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа деления двух чисел: введите два числа(делимое и деллитель)");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                int z = a / b;
+                Console.WriteLine($"{a}/{b}={z}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Возникло исключение DivideByZeroException(деление на ноль)");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
